ebe69042e3199f54f1e924f3b282e3aa5960bf1d
teejaymoo <teajay.m88@gmail.com>
2021-03-25 13:41:05 -0500

    Feat: Added Jumbotron

--- src/App.js
+++ src/App.js
@@ -58,6 +58,15 @@ class App extends Component {
             deleteAlert={this.deleteAlert}
           />
         ))}
+        <div className="jumbotron">
+          <h1 className="display-4">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-4"/>
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div>
         <main className="container">
           <Route path='/sign-up' render={() => (
             <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
